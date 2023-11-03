77d65e832d92d9a8528b2ef1967ebc695adac0e6
AbdisalaanAdan <147967989+shaakiraadan@users.noreply.github.com>
2023-11-02 19:40:42 -0930

    Add files via upload

---
+++ asiment todo list/shaka.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+// Route handler for the root URL
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
